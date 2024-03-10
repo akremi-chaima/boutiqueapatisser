754905f5d63d6adf7708fcd350e90cf0eda4c7ef
Chaima <chaima.akremi.1997@gmail.com>
2024-03-10 21:49:20 +0100

    [EVOL] - Handle user address

--- resources/src/Serializer/User/UserNormalizer.php
+++ resources/src/Serializer/User/UserNormalizer.php
@@ -2,11 +2,24 @@
 
 namespace App\Serializer\User;
 
+use App\Entity\Address;
 use App\Entity\User;
+use App\Manager\AddressManager;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class UserNormalizer implements NormalizerInterface
 {
+    /* @var AddressManager */
+    private $addressManager;
+
+    /**
+     * @param AddressManager $addressManager
+     */
+    public function __construct(AddressManager $addressManager)
+    {
+        $this->addressManager = $addressManager;
+    }
+
     /**
      * @param User $user
      * @param string|null $format
@@ -15,6 +28,9 @@ class UserNormalizer implements NormalizerInterface
      */
     public function normalize($user, string $format = null, array $context = [])
     {
+        /** @var Address|null $address */
+        $address = $this->addressManager->findOneBy(['user' => $user]);
+
         return [
             'id' => $user->getId(),
             'firstName' => $user->getFirstName(),
@@ -22,7 +38,10 @@ class UserNormalizer implements NormalizerInterface
             'password' => $user->getPassword(),
             'email' => $user->getEmail(),
             'phoneNumber' => $user->getPhoneNumber(),
-            'role' => $user->getRoles()[0]
+            'role' => $user->getRoles()[0],
+            'city' => $address->getCity(),
+            'zipCode' => $address->getZipCode(),
+            'street' => $address->getStreet()
         ];
     }
 
